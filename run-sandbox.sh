34686098e07a825a8e1cfa29dcac1438984129ba
nycnewman <edward@digitalasset.com>
2021-03-23 18:40:41 -0400

    Changes - PostGresQL Hardening - Minor fix for UI auth file

--- run-sandbox.sh
+++ run-sandbox.sh
@@ -44,7 +44,7 @@ fi
 java -jar daml-on-sql-1.10.0.jar \
  ./dist/ex-secure-daml-infra-0.0.1.dar \
  --client-auth $CLIENT_CERT_AUTH_PARAM \
- --sql-backend-jdbcurl "jdbc:postgresql://localhost/postgres?user=postgres&password=ChangeDefaultPassword!&ssl=on" \
+ --sql-backend-jdbcurl "jdbc:postgresql://db.$DOMAIN/ledger?user=ledger&password=LedgerPassword!&ssl=true&sslmode=verify-full&sslrootcert=$ROOTDIR/certs/intermediate/certs/ca-chain.cert.pem&sslcert=$ROOTDIR/certs/client/client1.$DOMAIN.cert.der&sslkey=$ROOTDIR/certs/client/client1.$DOMAIN.key.der" \
  $SIGNER_URL \
  --log-level DEBUG \
  --ledgerid $LEDGER_ID \
